5a03ddeb7487cc2c79d830ef5b9da3a0ebae693b
Vanya Kovalenko <mister.kat636@gmail.com>
2025-07-24 15:54:54 +0300

    upd 14

---
+++ scripts/typetester.js
@@ -0,0 +1,12 @@
+function updateSize(newVal){
+    var newFontSize = newVal + 'px';
+    $('.sample').css('font-size', newFontSize);
+  }
+  
+  
+  function updateFont(newVal) {
+    var newVale = newVal;
+    $('.sample').css('font-weight', newVal);
+  }
+  
+  
